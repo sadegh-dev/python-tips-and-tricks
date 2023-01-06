874801d522d029d71f049cf24697a57b9aa30d03
sadegh-dev <sadegh.rez@gmail.com>
2023-01-06 09:36:25 +0330

    docstring -edit 2

--- general/docstring.py
+++ general/docstring.py
@@ -1,9 +1,42 @@
 """
-    Multi-line descriptions are called Docstring.
-    
-    Docstring is placed at the highest level of function or class.
+    We write Docstring to explain the function or class
 
+    Multi-line descriptions are called Docstring.
 """
 
 
 
+
+"""
+    Docstring is placed at the highest level of function or class.
+
+    - The first line should be a brief description 
+    that starts with a capital letter 
+    and then the sentence ends with a Point.
+
+    - The second line is empty.
+
+    - The third line should include additional explanations.
+
+"""
+
+class Person:
+    """this is Docstring for Person class
+    """
+
+def doing(name):
+    """this docstring for doing function
+    """
+
+
+
+
+
+"""
+    In the Python code with the following command, 
+    this description will be displayed.
+"""
+
+help(Person)
+
+help(doing)

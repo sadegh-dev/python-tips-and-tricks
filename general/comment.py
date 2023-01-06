66586f3f555cd5517e257e1c417e359f1cad3470
sadegh-dev <sadegh.rez@gmail.com>
2023-01-06 09:44:38 +0330

    comment

---
+++ general/comment.py
@@ -0,0 +1,13 @@
+"""
+    The sun should not be in front of the code line, but above it.
+
+    comment is written in two ways.
+"""
+
+# This is a one-line comment.
+
+"""
+This is 
+a multi-line 
+explanation.
+"""

fe119dc828767541db625310ecc9692ad385dff8
sadegh-dev <sadegh.rez@gmail.com>
2023-01-09 13:53:36 +0330

    features of the methods

---
+++ general/difference-between-function-and-method.py
@@ -0,0 +1,14 @@
+"""
+    * good reference:
+        https://www.geeksforgeeks.org/difference-method-function-python/
+
+"""
+
+"""
+    + features of the METHODS:
+        - the METHODS is always called under one OBJECT.
+
+
+"""
+
+

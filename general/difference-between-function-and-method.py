92d819575c66f0aa7268f5d370bc96b92e4b69a5
sadegh-dev <sadegh.rez@gmail.com>
2023-01-09 14:02:48 +0330

    features of the function

--- general/difference-between-function-and-method.py
+++ general/difference-between-function-and-method.py
@@ -2,13 +2,24 @@
     * good reference:
         https://www.geeksforgeeks.org/difference-method-function-python/
 
-"""
 
-"""
     + features of the METHODS:
-        - the METHODS is always called under one OBJECT.
 
+        - The METHODS is always dependent on an OBJECT and is called under it.
+        
+        - the METHODS always receives the SELF as an input.
+
+
+    + features of the FUNCTION:
+
+        - There are blocks of codes that do not depend on anything 
+        and are called completely independently.
+        
+        - FUNCTIONS can take different parameters or no parameters at all.
+        
+        - FUNCTIONS have no dependence on the CLASS or its INSTANCE.
 
 """
+
 
 

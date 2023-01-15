d0528797326cea3ddf0a916ddb67d4eb23d03ae3
sadegh-dev <sadegh.rez@gmail.com>
2023-01-15 07:50:55 +0330

    ex1 on lambda-function.py"

--- lambda/lambda-function.py
+++ lambda/lambda-function.py
@@ -10,3 +10,35 @@
 
         - The answer must be used by : list(map(lambda ...) )
 """
+
+# name_function = lambda data_in : doing on data_inputs
+
+
+
+########
+# EX 1 #
+########
+
+# normal 
+
+def add(x,y):
+    return x+y
+
+
+# lambda
+
+add = lambda x,y : x+y
+
+print(add(2,5))
+print(add(33,46))
+
+
+
+
+
+
+
+
+
+
+

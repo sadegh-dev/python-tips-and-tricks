57d6b7e8a9136366ded044c37306816322a3402b
sadegh-dev <sadegh.rez@gmail.com>
2023-01-15 07:42:39 +0330

    write comment in lambda-function.py

---
+++ lambda/lambda-function.py
@@ -0,0 +1,12 @@
+"""
+    These functions return the results automatically 
+    and there is no need to write the RETURN keyword.
+"""
+
+"""
+    Work on the list :
+        - If the input of lambda is a list,
+        its output is also a list.
+
+        - The answer must be used by : list(map(lambda ...) )
+"""

c9a4f3c97f9772800175b0c8673274643661c8e1
sadegh-dev <sadegh.rez@gmail.com>
2023-02-08 18:13:45 +0330

    description variable naming rules

---
+++ variable/variable-naming-rules.py
@@ -0,0 +1,25 @@
+"""
+
+    - names of variables and functions are case-sensitive,
+     for example the two names Age and age are different.
+    
+    - names of variables and functions can only 
+    contain uppercase and lowercase letters, 
+    numbers and _underscore: [A-z, 0-9, _]
+    
+    - names cannot contain characters, like: !@#$%^&*
+    
+    - names of variables and functions cannot 
+    start with numbers or contain spaces.
+     
+    - names of variables and functions cannot 
+    be reserved names in python, such as: int, str
+     
+    - it is better for the variable name to describe its contents, 
+    the name of the function describes what it does, 
+    for example, age is used to store the age of people.
+     
+    - it is better for variables that are of boolean type, 
+    to start their names with is_ .
+
+"""

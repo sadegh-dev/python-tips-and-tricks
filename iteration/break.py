b335eb0d7093e5bd800ed9277b196a867a60770a
sadegh-dev <sadegh.rez@gmail.com>
2023-02-11 13:02:07 +0330

    ex1 for iteration>break

--- iteration/break.py
+++ iteration/break.py
@@ -1,8 +1,17 @@
 """
-    * The 'break' statement is a way to exit the loop 
+    * the 'break' statement is a way to exit the loop 
     and stop it completely.
 
-    * The 'break' command should be placed in an 'if', 
+    * the 'break' command should be placed in an 'if', 
     so that if the condition is met, it will stop the loop execution.
 
 """
+
+
+# EX 1 :
+
+for x in range(10):
+    if x == 5 :
+        print("the loop stopped.")
+        break
+    print(x)

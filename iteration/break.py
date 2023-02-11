205e37653ec6eb3e52203e496f2e4495fcc77115
sadegh-dev <sadegh.rez@gmail.com>
2023-02-11 12:58:04 +0330

    description break command

---
+++ iteration/break.py
@@ -0,0 +1,8 @@
+"""
+    * The 'break' statement is a way to exit the loop 
+    and stop it completely.
+
+    * The 'break' command should be placed in an 'if', 
+    so that if the condition is met, it will stop the loop execution.
+
+"""

29f27fbd99d097115faf39efeb3c30b6a5366ada
sadegh-dev <sadegh.rez@gmail.com>
2023-02-11 13:13:00 +0330

    description of iteration>continue

---
+++ iteration/continue.py
@@ -0,0 +1,8 @@
+"""
+    * the continue command is used to stop a step of the execution of the loop instruction.
+
+    * the continue command must be placed inside a conditional command 
+    so that when that condition occurs, 
+    the computer command is executed and that the loop step is not executed.
+
+"""
